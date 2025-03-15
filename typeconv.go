ea13cafaf717f93184472044e36641a35092b5bd
Jeffrey Buchbinder <jbuchbinder@ravemobilesafety.com>
2025-03-15 14:08:01 -0400

    TypeConv: +ArrayConvertNoEmpty

--- typeconv.go
+++ typeconv.go
@@ -1,11 +1,26 @@
 package shims
 
+import "reflect"
+
 // ArrayConvert converts an array of one type to an array of another via an
 // arbitrary transform function.
 func ArrayConvert[F any, T any](in []F, f func(F) T) []T {
 	out := make([]T, 0)
 	for _, v := range in {
 		out = append(out, f(v))
+	}
+	return out
+}
+
+// ArrayConvertNoEmpty converts an array of one type to an array of another via an
+// arbitrary transform function, disregarding null values.
+func ArrayConvertNoEmpty[F any, T any](in []F, f func(F) T) []T {
+	out := make([]T, 0)
+	for _, v := range in {
+		x := f(v)
+		if !reflect.DeepEqual(x, reflect.Zero(reflect.TypeOf(x)).Interface()) {
+			out = append(out, f(v))
+		}
 	}
 	return out
 }
